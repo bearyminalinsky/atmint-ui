02ab023ed76bbd9236fb2936d0685f783909f402
bearyminalinsky <111202113359@mhs.dinus.ac.id>
2024-04-27 22:31:06 +0700

    tugas 3 syarat uts

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,52 +1,63 @@
 import "./sidebar.scss";
 import { Link } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AddCardIcon from '@mui/icons-material/AddCard';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-const Sidebar = () =>
-{
-    return (
-        <div className="sidebar">
-        <div className="top">
-          <Link to="/">
-            <span className="logo">Store</span>
-          </Link>
-        </div>
-        <hr />
-        <div className="center">
-          <ul>
-            <p className="title">MAIN</p>
-            <li>
-              <span>Dashboard</span>
-            </li>
-            <p className="title">LIST</p>
-            <Link to="/users">
-              <li>
-                <span>Users</span>
-              </li>
-            </Link>
-            <Link to="/products">
-              <li>
-                <span>Products</span>
-              </li>
-            </Link>
-            <li>
-              <span>Orders</span>
-            </li>
-            <p className="title">USER</p>
-            <li>
-              <span>Profile</span>
-            </li>
-            <li>
-              <span>Logout</span>
-            </li>
-          </ul>
-        </div>
-        <div className="bottom">
-          <div className="colorOption"></div>
-          <div className="colorOption"></div>
-        </div>
-      </div>
-    );
+const Sidebar = () => {
+  return (
+    
+<div className="sidebar">
+<div className="top">
+  <Link to="/">
+    <span className="logo">Store</span>
+  </Link>
+</div>
+<hr />
+<div className="center">
+  <ul>
+    <p className="title">MAIN</p>
+    <li>
+        <DashboardIcon className="icon"/>
+      <span>Dashboard</span>
+    </li>
+    <p className="title">LIST</p>
+    <Link to="/users">
+      <li>
+      <AccountCircleIcon className="icon"/>
+        <span>Users</span>
+      </li>
+    </Link>
+    <Link to="/products">
+      <li>
+      <AddCardIcon className="icon"/>
+        <span>Products</span>
+      </li>
+    </Link>
+    <li>
+    <AddBusinessIcon className="icon"/>
+      <span>Orders</span>
+    </li>
+    <p className="title">USER</p>
+    <li>
+    <PersonIcon className="icon"/>
+      <span>Profile</span>
+    </li>
+    <li>
+    <LogoutIcon className="icon"/>
+      <span>Logout</span>
+    </li>
+  </ul>
+</div>
+<div className="bottom">
+  <div className="colorOption"></div>
+  <div className="colorOption"></div>
+</div>
+</div>
+  );
 };
 
 export default Sidebar;
